439c627d0b2717192c22cd3b48f8d8fe2097fcbb
AmirShariat <amishari@yahoo.com>
2023-02-08 20:47:43 +0330

    state management for Modal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,12 +4,12 @@ import HeaderCart from "./HeaderCart";
 import mealsImage from "../../asset/meal.jpg";
 import classes from "./Header.module.css";
 
-const Header = () => {
+const Header = ({ onShowCard }) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCart />
+        <HeaderCart onClick={onShowCard} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
--- src/components/Layout/HeaderCart.js
+++ src/components/Layout/HeaderCart.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { FaCartArrowDown } from "react-icons/fa";
 import classes from "./HeaderCart.module.css";
-export default function HeaderCart() {
+
+const HeaderCart = ({ onClick }) => {
   return (
     <div>
-      <button className={classes.button}>
+      <button className={classes.button} onClick={onClick}>
         <span className={classes.icon}>
           <FaCartArrowDown size={30} />
         </span>
-        <span>Your</span>
+        <span>Your Cart</span>
         <span className={classes.badge}>3</span>
       </button>
     </div>
   );
-}
+};
+export default HeaderCart;
