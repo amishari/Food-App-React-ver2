da77fce6bec450e19227692fa15704c40a28c00f
amishari <81028814+amishari@users.noreply.github.com>
2023-02-08 07:51:26 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Header from "./components/Head/Header";
+import Cart from "./components/Head/Cart";
+
+function App() {
+  return (
+    <div className="App">
+      <Cart />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
