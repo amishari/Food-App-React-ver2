9dbec37bbcfde8df1297f81bc4ec3afc5b394b9a
amishari <81028814+amishari@users.noreply.github.com>
2023-02-09 11:07:36 +0330

    Update Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,12 +4,12 @@ import HeaderCart from "./HeaderCart";
 import mealsImage from "../../asset/meal.jpg";
 import classes from "./Header.module.css";
 
-const Header = ({ onShowCard }) => {
+const Header = ({ onShowCart }) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCart onClick={onShowCard} />
+        <HeaderCart onClick={onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
